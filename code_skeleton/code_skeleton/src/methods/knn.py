e22d6b5803079e7c31f4b5cb7fb549dcf434b3ba
Serhii Akimov <serhii.akimov@epfl.ch>
2026-04-17 02:04:29 +0200

    Added kNN method
    Implemted kNN method inspired by our labs, my kNN doesn't normalize data

--- code_skeleton/code_skeleton/src/methods/knn.py
+++ code_skeleton/code_skeleton/src/methods/knn.py
@@ -28,13 +28,62 @@ class KNN(object):
         Returns:
             pred_labels (np.array): labels of shape (N,)
         """
+        self.training_data = training_data
+        self.training_labels = training_labels
+        return self.training_labels
+    
 
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
-        return pred_labels
+    def predict_label(self, neighbor_labels):
+        """Return the most frequent label in the neighbors'.
+
+        Inputs:
+            neighbor_labels: shape (N,) 
+        Outputs:
+            most frequent label
+        """
+        if self.task_kind == "classification":
+                return np.argmax(np.bincount(neighbor_labels.astype(int)))
+        if self.task_kind == "regression":   
+            return np.mean(neighbor_labels)
+        
+    def find_k_nearest_neighbors(self, distances):
+        """ Find the indices of the k smallest distances from a list of distances.
+            Tip: use np.argsort()
+
+        Inputs:
+            k: integer
+            distances: shape (N,) 
+        Outputs:
+            indices of the k nearest neighbors: shape (k,)
+        """
+        indices = np.argsort(distances)
+        return indices[:self.k]
+    
+    def kNN_one_example(self, unlabeled_example):
+        """Returns the label of a single unlabelled example.
+
+        Inputs:
+            unlabeled_example: shape (D,) 
+            training_features: shape (NxD)
+            training_labels: shape (N,) 
+            k: integer
+        Outputs:
+            predicted label
+        """
+        # find distance of the single test example w.r.t. all training examples
+        root = np.square(unlabeled_example - self.training_data)
+        distances = np.sqrt(np.sum(root, axis = 1))
+        
+        # find the nearest neighbors
+        nn_indices = self.find_k_nearest_neighbors(distances)
+        
+        # find the labels of the nearest neighbors
+        neighbor_labels = self.training_labels[nn_indices]
+        
+        # Pick the most common
+        best_label = self.predict_label(neighbor_labels)
+        
+        return best_label
 
     def predict(self, test_data):
         """
@@ -45,9 +94,6 @@ class KNN(object):
         Returns:
             test_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
-        return test_labels
+
+        return np.apply_along_axis(func1d= self.kNN_one_example, axis=1, arr=test_data)
+    
